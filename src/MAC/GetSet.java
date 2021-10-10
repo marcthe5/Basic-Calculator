0fe0d3737052f4f04e543bca38ae289d459adb98
Casoco, Marc T <marccasoco@gmail.com>
2021-10-10 20:04:56 +0800

    Update GetSet.java

--- src/MAC/GetSet.java
+++ src/MAC/GetSet.java
@@ -1,5 +1,15 @@
 package MAC;
 
 public class GetSet {
-
+	
+	protected int num_input1;
+	protected int num_input2;
+	
+	
+	public void setNum1(int num_input1) {
+    this.num_input1 = num_input1;
+}
+	public int getNum1() {
+		return num_input1;
+	}
 }
